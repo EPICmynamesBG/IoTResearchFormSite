bdce0c0cae7dc31de719a9f3fc41731b5d26f40b
Brandon Groff <mynamesbg@gmail.com>
2016-10-09 16:12:54 -0400

    Stylized and functioning input form. TODO: Dropdowns of already submitted items

--- frontend/scripts/controllers/InputFormController.js
+++ frontend/scripts/controllers/InputFormController.js
@@ -1,13 +1,58 @@
 app.controller('InputFormController', ['$scope', '$state', 'API', function ($scope, $state, API) {
 
-    $scope.categories = [];
+    var initialize = function () {
+        $scope.categories = [];
+        $scope.input = {};
+        $scope.input.categories = [];
+        $scope.disableAddFileButton = true;
+        $scope.urlEntries = [null];
+    };
+    initialize();
 
-    console.log(API);
+    $scope.urlTyping = function (index, newtext) {
+        if (!newtext || newtext.length < 4) {
+            $scope.urlEntries[index] = null;
+            $scope.disableAddFileButton = true;
+        } else {
+            $scope.urlEntries[index] = newtext;
+            $scope.disableAddFileButton = false;
+        }
+    };
+
+    $scope.addFileUrl = function () {
+        var lastItem = $scope.urlEntries[$scope.urlEntries.length - 1];
+        if (lastItem.length != null) {
+            $scope.urlEntries.push(null);
+            $scope.disableAddFileButton = true;
+        }
+    };
+
+    $scope.toggleSelection = function (category) {
+        var found = $scope.input.categories.indexOf(category.id);
+        if (found == -1) {
+            $scope.input.categories.push(category.id);
+        } else {
+            $scope.input.categories.splice(found, 1);
+        }
+    };
+
     API.category.getAll()
         .then(function (data) {
             $scope.categories = data.data.data;
         }, function (error) {
             console.log(error);
         });
+
+    $scope.submit = function () {
+        $scope.input.files = $scope.urlEntries;
+        API.observation.create($scope.input)
+            .then(function (data) {
+                console.log(data);
+                alert("Success");
+                initialize();
+            }, function (error) {
+                console.log(error);
+            });
+    }
 
 }]);
--- frontend/scripts/factories/API.js
+++ frontend/scripts/factories/API.js
@@ -3,7 +3,7 @@ app.factory('API', ['config', '$http', function (config, $http) {
     var factory = {};
 
     var observation = {};
-    
+
     var category = {};
 
     observation.getAll = function () {
@@ -12,32 +12,33 @@ app.factory('API', ['config', '$http', function (config, $http) {
             url: config.url + '/observations'
         });
     };
-    
+
     observation.getById = function (id) {
         return $http({
             method: 'GET',
             url: config.url + '/observation/' + id
         });
     };
-    
+
     observation.create = function (obsData) {
+        var data = {
+            user_1: obsData.user_1,
+            user_2: !obsData.user_2 ? undefined : obsData.user_2,
+            device: obsData.device,
+            categories: obsData.categories,
+            tool: obsData.tool,
+            toolParams: obsData.toolParams,
+            observations: obsData.observations,
+            implications: !obsData.implications ? undefined : obsData.implications,
+            files: !obsData.files || obsData.files.length == 0 ? undefined : obsData.files
+        };
         return $http({
             method: 'POST',
             url: config.url + '/observation',
             headers: {
                 'Content-Type': 'application/json'
             },
-            data: {
-                user_1: obsData.user1,
-                user_2: !obsData.user2 ? undefined : obsData.user2,
-                device: obsData.device,
-                categories: obsData.categories,
-                tool: obsData.tool,
-                toolParams: obsData.toolParams,
-                observations: obsData.observations,
-                implications: !obsData.implications ? undefined : obsData.implications,
-                files: !obsData.files || obsData.files.length == 0 ? undefined : obsData.files
-            }
+            data: data
         });
     };
 
@@ -47,8 +48,8 @@ app.factory('API', ['config', '$http', function (config, $http) {
             url: config.url + '/categories'
         });
     };
-    
-    
+
+
     factory.observation = observation;
     factory.category = category;
     return factory;
