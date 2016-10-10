7360ac59079359807ff8132d89403449429adcba
Brandon Groff <mynamesbg@gmail.com>
2016-10-10 17:52:28 -0400

    Update prod configs

--- backend/src/config/db_configs/prod.php
+++ backend/src/config/db_configs/prod.php
@@ -1,6 +1,6 @@
 <?php
 $CONFIG['server_name'] = "localhost";
-$CONFIG['server_user'] = "root";
-$CONFIG['server_pass'] = "root";
+$CONFIG['server_user'] = "iot_research";
+$CONFIG['server_pass'] = "exhJEzDvhrRsr29b";
 $CONFIG['server_db'] = "iot_research";
 $CONFIG['server_port'] = 8889;
